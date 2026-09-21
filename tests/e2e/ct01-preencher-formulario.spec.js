54c6cb8fa4bd72ed3df575db5e6a4d3fb7e83103
Tiago Freitas <tiago.freitas.sp@gmail.com>
2026-09-21 11:45:30 -0300

    Criado cenario CT01

--- tests/e2e/ct01-preencher-formulario.spec.js
+++ tests/e2e/ct01-preencher-formulario.spec.js
@@ -22,14 +22,47 @@ for (const lineFromExcel of ExcelDataProvider) {
         test(`${lineFromExcel.ID} - ${lineFromExcel.CENARIO} ${num++}`, 
             {
                 tag: [
-                    '@form',
+                    '@ct01',
                     '@regressivo',
-                    '@ct01'
+                    '@form'
                 ],
             }, async ({ page }) => {
 
             preencherFormularioPage = new PreencherFormularioPage(page);
             await preencherFormularioPage.abrirNavegador(process.env.BASE_URL);
+            await preencherFormularioPage.preencherFormulario(
+                lineFromExcel.FIRST_NAME,
+                lineFromExcel.LAST_NAME,
+                lineFromExcel.EMAIL,
+                lineFromExcel.GENDER,
+                lineFromExcel.MOBILE_NUMBER,
+                lineFromExcel.DAY_OF_BIRTH,
+                lineFromExcel.MONTH_OF_BIRTH,
+                lineFromExcel.YEAR_OF_BIRTH,
+                lineFromExcel.SUBJECTS,
+                lineFromExcel.HOBBIES,
+                lineFromExcel.PICTURE,
+                lineFromExcel.ADDRESS,
+                lineFromExcel.STATE,
+                lineFromExcel.CITY
+            );
+            await preencherFormularioPage.clicarBotaoSubmit();
+            await preencherFormularioPage.validarFormularioSubmetido(
+                lineFromExcel.FIRST_NAME,
+                lineFromExcel.LAST_NAME,
+                lineFromExcel.EMAIL,
+                lineFromExcel.GENDER,
+                lineFromExcel.MOBILE_NUMBER,
+                lineFromExcel.DAY_OF_BIRTH,
+                lineFromExcel.MONTH_OF_BIRTH,
+                lineFromExcel.YEAR_OF_BIRTH,
+                lineFromExcel.SUBJECTS,
+                lineFromExcel.HOBBIES,
+                lineFromExcel.PICTURE,
+                lineFromExcel.ADDRESS,
+                lineFromExcel.STATE,
+                lineFromExcel.CITY
+            );
         });
     }
 }
--- tests/page/PreencherFormularioPage.js
+++ tests/page/PreencherFormularioPage.js
@@ -12,8 +12,103 @@ export class PreencherFormularioPage{
     }
 
     async abrirNavegador(url){
-        await this.page.goto(url);
-        await this.page.waitForLoadState('networkidle')
+        await this.page.goto(url, { waitUntil: 'networkidle' });
         await expect(this.page).toHaveTitle(/demosite/);
+    }
+
+    async preencherFormulario(FIRST_NAME, LAST_NAME, EMAIL, GENDER, MOBILE_NUMBER, DAY_OF_BIRTH, MONTH_OF_BIRTH, YEAR_OF_BIRTH, SUBJECTS, HOBBIES, PICTURE, ADDRESS, STATE, CITY){
+        await this.validarTituloFormulario();
+        await this.validarSubtituloFormulario();
+        await this.inserirPrimeiroNome(FIRST_NAME);
+        await this.inserirUltimoNome(LAST_NAME);
+        await this.inserirEmail(EMAIL);
+        await this.inserirGenero(GENDER);
+        await this.inserirNumeroCelular(MOBILE_NUMBER);
+        await this.inserirDataDeNascimento(DAY_OF_BIRTH, MONTH_OF_BIRTH, YEAR_OF_BIRTH);
+        await this.inserirSubjects(SUBJECTS);
+        await this.inserirHobbies(HOBBIES);
+        //await this.inserirPicture(PICTURE);
+        await this.inserirAddress(ADDRESS);
+        await this.inserirState(STATE);
+        await this.inserirCity(CITY);
+    }
+
+    async validarTituloFormulario(){
+        await base.toBeVisible('//h1[normalize-space(text())=\'Practice Form\']');
+    }
+
+    async validarSubtituloFormulario(){
+        await base.toBeVisible('//h5[normalize-space(text())=\'Student Registration Form\']');
+    }
+
+    async inserirPrimeiroNome(FIRST_NAME){
+        await base.fill('//input[@id=\'firstName\']', FIRST_NAME);
+    }
+    
+    async inserirUltimoNome(LAST_NAME){
+        await base.fill('//input[@id=\'lastName\']', LAST_NAME);
+    }
+    
+    async inserirEmail(EMAIL){
+        await base.fill('//input[@id=\'userEmail\']', EMAIL);
+    }
+
+    async inserirGenero(GENDER){
+        await base.click(`//input[@name='gender' and @value='${GENDER}']`);
+    }
+
+    async inserirNumeroCelular(MOBILE_NUMBER){
+        await base.fill('//input[@id=\'userNumber\']', MOBILE_NUMBER);
+    }
+
+    async inserirDataDeNascimento(DAY_OF_BIRTH, MONTH_OF_BIRTH, YEAR_OF_BIRTH){
+        await base.fill('//input[@id=\'dateOfBirthInput\']', `${DAY_OF_BIRTH} ${MONTH_OF_BIRTH} ${YEAR_OF_BIRTH}`);
+    }
+
+    async inserirSubjects(SUBJECTS){
+        await base.fill('//input[@id=\'subjectsInput\']', SUBJECTS);
+    }
+
+    async inserirHobbies(HOBBIES){
+        for(const hobby of HOBBIES.split(',')){
+            await base.click(`//label[normalize-space(text())='${hobby.trim()}']`);
+        }
+    }
+
+    async inserirPicture(PICTURE){
+        await base.setInputFiles('//input[@id=\'uploadPicture\']', PICTURE);
+    }
+
+    async inserirAddress(ADDRESS){
+        await base.fill('//textarea[@id=\'currentAddress\']', ADDRESS);
+    }
+
+    async inserirState(STATE){
+        await base.click('//div[@id=\'state\']');
+        await base.click(`//div[@id='state']//div[text()='${STATE}']`);
+    }
+
+    async inserirCity(CITY){
+        await base.click('//div[@id=\'city\']');
+        await base.click(`//div[@id='city']//div[text()='${CITY}']`);
+    }
+
+    async clicarBotaoSubmit(){
+        await base.click('#submit');
+    }
+
+    async validarFormularioSubmetido(FIRST_NAME, LAST_NAME, EMAIL, GENDER, MOBILE_NUMBER, DAY_OF_BIRTH, MONTH_OF_BIRTH, YEAR_OF_BIRTH, SUBJECTS, HOBBIES, PICTURE, ADDRESS, STATE, CITY){
+        await base.toBeVisible(`//td[normalize-space(text())='${FIRST_NAME}']`);
+        await base.toBeVisible(`//td[normalize-space(text())='${LAST_NAME}']`);
+        await base.toBeVisible(`//td[normalize-space(text())='${EMAIL}']`);
+        await base.toBeVisible(`//td[normalize-space(text())='${GENDER}']`);
+        await base.toBeVisible(`//td[normalize-space(text())='${MOBILE_NUMBER}']`);
+        await base.toBeVisible(`//td[normalize-space(text())='${DAY_OF_BIRTH} ${MONTH_OF_BIRTH} ${YEAR_OF_BIRTH}']`);
+        await base.toBeVisible(`//td[normalize-space(text())='${SUBJECTS}']`);
+        await base.toBeVisible(`//td[normalize-space(text())='${HOBBIES}']`);
+        //await base.toBeVisible(`//td[normalize-space(text())='${PICTURE}']`);
+        await base.toBeVisible(`//td[normalize-space(text())='${ADDRESS}']`);
+        await base.toBeVisible(`//td[normalize-space(text())='${STATE}']`);
+        await base.toBeVisible(`//td[normalize-space(text())='${CITY}']`);
     }
 }
